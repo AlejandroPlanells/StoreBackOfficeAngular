40bb854642932601efc735ec72ada44c4a5f7f94
alex10planells@gmail.com <Pucheritoo15*>
2022-05-16 11:58:58 +0200

    add: structure project (+bootstrap)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './entities/category/category-list/category-list.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'categories', component: CategoryListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
